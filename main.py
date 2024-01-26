85d0b11c110beacd6599ce39654c362aa4e69307
K4RF <rpdirwk1025@naver.com>
2024-01-26 20:45:35 +0900

    feat: 봇 on/off 시 메시지 보내는 기능 추가

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 from config import TOKEN, TARGET_USERS, TARGET_WORDS
-from datetime import datetime, timedelta
 import asyncio
 
 intents = discord.Intents.default()
@@ -12,19 +11,27 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+# 특정 채널 ID
+target_channel_id = 1163856267756970124  # 실제 채널 ID로 변경해야 합니다.
+
 # 멤버별 보낸 횟수를 저장할 딕셔너리
 sent_count_data = {}
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name}이(가) 성공적으로 로그인했습니다!')
+    
+    # 봇이 켜질 때 메시지를 특정 채널에 보냅니다.
+    target_channel = bot.get_channel(target_channel_id)
+    if target_channel:
+        await target_channel.send(f'계엄사령부에서 알려드립니다. 계엄령이 선포되었습니다')
 
-async def send_delayed_message(member, content):
-    await asyncio.sleep(60 * 60)  # 60분
-    try:
-        await member.send(content)
-    except discord.errors.Forbidden:
-        pass  # DM이 차단되어 있는 경우 무시
+@bot.event
+async def on_disconnect():
+    # 봇이 꺼질 때 메시지를 특정 채널에 보냅니다.
+    target_channel = bot.get_channel(target_channel_id)
+    if target_channel:
+        await target_channel.send(f'계엄령이 해제되었습니다.')
 
 @bot.event
 async def on_message_delete(message):
@@ -32,17 +39,11 @@ async def on_message_delete(message):
     member = message.guild.get_member(message.author.id)
     if member and member not in sent_count_data:
         try:
-            await message.channel.send(f'{message.author.mention}, 봇이 삭제한 메시지를 확인해주세요. '
-                                       f'이 메시지는 최초 삭제 시에만 봇이 보냅니다.')
+            await message.channel.send(f'{message.author.mention}, 봇이 삭제한 메시지를 확인해주세요.')
         except discord.errors.NotFound:
             pass  # 채널이 이미 삭제된 경우 무시
 
         # 보낸 횟수를 업데이트
         sent_count_data[member] = 1
-
-        # 최초 삭제 시에만 멤버에게 다시 메시지 보내기
-        delayed_message = f'{message.author.mention}, 봇이 삭제한 메시지를 확인해주세요. ' \
-                          f'이 메시지는 최초 삭제 시에만 봇이 보냅니다.'
-        bot.loop.create_task(send_delayed_message(member, delayed_message))
 
 bot.run(TOKEN)
