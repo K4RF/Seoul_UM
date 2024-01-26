defb67446424891d1fe83df99fc7be1cb25abf13
K4RF <rpdirwk1025@naver.com>
2024-01-26 18:39:03 +0900

    fix: 멤버별 최초 삭제로 변경
    - 멤버별로 최초 삭제 시에 삭제 메시지를 보내도록 변경

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# 사용자별 처음 삭제 시간을 저장할 딕셔너리
+# 멤버별 처음 삭제 시간을 저장할 딕셔너리
 first_deletion_time = {}
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name}이(가) 성공적으로 로그인했습니다!')
 
-async def send_delayed_message(author, content):
+async def send_delayed_message(member, content):
     await asyncio.sleep(60 * 60)  # 60분
     try:
-        await author.send(content)
+        await member.send(content)
     except discord.errors.Forbidden:
         pass  # DM이 차단되어 있는 경우 무시
 
@@ -31,8 +31,9 @@ async def on_message(message):
     if message.author.id in TARGET_USERS:
         current_time = datetime.utcnow()
 
-        # 사용자가 처음으로 메시지를 삭제한 시간
-        first_time = first_deletion_time.get(message.author.id)
+        # 멤버가 처음으로 메시지를 삭제한 시간
+        member = message.guild.get_member(message.author.id)
+        first_time = first_deletion_time.get(member)
 
         # 금지어가 포함된 경우만 처리
         for target_word in TARGET_WORDS:
@@ -50,12 +51,12 @@ async def on_message(message):
                         pass  # 채널이 이미 삭제된 경우 무시
 
                     # 현재 시간으로 업데이트
-                    first_deletion_time[message.author.id] = current_time
+                    first_deletion_time[member] = current_time
 
-                    # 최초 삭제 시에만 사용자에게 다시 메시지 보내기
+                    # 최초 삭제 시에만 멤버에게 다시 메시지 보내기
                     delayed_message = f'{message.author.mention}, 이 메시지는 금지어가 포함되어 삭제되었습니다. ' \
                                       f'최초 삭제 시에만 봇이 이 메시지를 보냅니다.'
-                    bot.loop.create_task(send_delayed_message(message.author, delayed_message))
+                    bot.loop.create_task(send_delayed_message(member, delayed_message))
 
                 return  # 다음 동작을 방지하기 위해 리턴
 
