03aec0ebee1598371ea3b62eb010fa85375232d7
Kaarf_o <rpdirwk1025@naver.com>
2024-01-26 15:28:56 +0900

    유저 ID 감춤

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 TOKEN = 'Bot_Token'
-TARGET_USERS = [404558066005180416, 712140650866540585, 383529679551725568, 346563993328025602, 292579015531692032, 864475917685096458]  # 삭제 대상 사용자들의 Discord ID 입력
+TARGET_USERS = [User, ID]  # 삭제 대상 사용자들의 Discord ID 입력
 TARGET_WORDS = ['엄', 'UM', 'úm']  # 삭제하고 싶은 단어나 문구 입력
 
 intents = discord.Intents.default()
